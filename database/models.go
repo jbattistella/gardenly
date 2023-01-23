5c50f53c4137504447aa2626ad22d16bf3bf2f93
Joseph Battistella <jopabatti@gmail.com>
2023-01-23 11:42:07 -0600

    add db handler

--- database/models.go
+++ database/models.go
@@ -1,8 +1,8 @@
 package database
 
 type Vegetable struct {
-	ID         int    `json:"id"`
+	// ID         int    `json:"id"`
 	CommonName string `json:"common_name"`
 	DTM        int    `json:"dtm"`
-	DownToTemp int    `json:"frost_temp"`
+	FrostTemp  int    `json:"frost_temp"`
 }
