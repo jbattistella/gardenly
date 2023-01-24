3835ed2a8aefa2ea0ecc1759f78160c646c18f6e
Joseph Battistella <jopabatti@gmail.com>
2023-01-24 10:50:31 -0600

    click tests/condition ref/html interate

--- database/models.go
+++ database/models.go
@@ -1,7 +1,6 @@
 package database
 
 type Vegetable struct {
-	// ID         int    `json:"id"`
 	CommonName string `json:"common_name"`
 	DTM        int    `json:"dtm"`
 	FrostTemp  int    `json:"frost_temp"`
